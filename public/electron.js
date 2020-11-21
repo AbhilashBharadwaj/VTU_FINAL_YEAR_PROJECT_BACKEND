7c0ad644db862e3751e23f3f1b9b9ddd5ca95b6d
AbhilashBharadwaj <abhilash.sampath@yahoo.in>
2020-11-21 15:57:01 +0530

    Update Public

---
+++ public/electron.js
@@ -0,0 +1,34 @@
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const isDev = require("electron-is-dev");
+let mainWindow;
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 750,
+    height: 650,
+  });
+  mainWindow.removeMenu();
+
+  mainWindow.loadURL(
+    // isDev ? "http://localhost:3000" : "http://localhost:3000"
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+  mainWindow.on("closed", () => (mainWindow = null));
+}
+app.commandLine.appendSwitch("ignore-certificate-errors", "true");
+app.on("ready", createWindow);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
